7a78b2a533988e7b981ac8bbb1e25451a7d185e7
cw1204772 <peter19940407@yahoo.com.tw>
2015-05-21 11:31:41 +0800

    Moving multiple functions to separate files
    Moving multiple functions into separate .cpp and .h

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include <cmath>
 #include "model.h"
 #include "framebuffer.h"
+#include "transform.h"
+#include "rasterize.h"
+#include "lighting.h"
 using namespace std;
 
 #define PI 3.14159265
@@ -102,291 +105,12 @@ void init()
 	culling = true;
 }
 
-vec3 transform(vec3& prev)
-{
-	float curX,curY,curZ;
-	// rotate along x-axis
-	curY = prev.y*cos(thetaX)-prev.z*sin(thetaX);
-	curZ = prev.y*sin(thetaX)+prev.z*cos(thetaX);
-	// prevY = curY;
-	prev.z = curZ;
-	// rotate along y-axis
-	curX = prev.z*sin(thetaY)+prev.x*cos(thetaY);
-	curZ = prev.z*cos(thetaY)-prev.x*sin(thetaY);
-	return vec3(curX,curY,curZ);
-}
-
-void toScreenSpace(vec3& v,int& ix,int& iy,float& iz)
-{
-	int screenSide = min(screenWidth, screenHeight);
-	float margin = 0.1f;
-	// to ScreenSpace
-	ix = int(((1.f-margin)*v.x)*screenSide) + screenWidth_half - 1;
-	iy = int(((1.f-margin)*v.y)*screenSide) + screenHeight_half - 1;
-	iz = v.z;
-}
-
-bool backFaceCulling(vec3* triangleV)   //input 3 vertex of the triangle
-{
-	vec3 v1(triangleV[0].x-triangleV[1].x,triangleV[0].y-triangleV[1].y,triangleV[0].z-triangleV[1].z);
-	vec3 v2(triangleV[0].x-triangleV[2].x,triangleV[0].y-triangleV[2].y,triangleV[0].z-triangleV[2].z);
-	vec3 v3 = crossProduct(v1,v2);
-	normalize(v3);
-	vec3 v4(0.f,0.f,-1.f);
-	//cout << dotProduct(v3,v4) << endl;
-	if (dotProduct(v3,v4) < 0) return true;
-	else return false;
-}
-
-vec3 lighting(vec3 normal)     //input 3 triangle vertex
-{
-	//ambient
-	float ambient_light_intensity =  1;
-	float ambient_reflection_coefficient = 0.2;
-	float ambient = ambient_light_intensity * ambient_reflection_coefficient;
-	//diffuse
-	float point_light_attenuation = 1;
-	float point_light_intensity = 1;
-	float diffuse_reflection_coefficient = 0.8;
-
-	normalize(normal);
-	vec3 light_direction(1.f,1.f,-2.f);
-	normalize(light_direction);
-	float diffuse = point_light_attenuation * point_light_intensity * diffuse_reflection_coefficient * dotProduct(normal,light_direction);
-	//specular
-	float specular = 0; //sum ()
-	float intensity = ambient + diffuse + specular;
-	vec3 c = color;
-	c.x *= intensity;
-	c.y *= intensity;
-	c.z *= intensity;
-	return c;
-}
-
-void drawStandingTriangle(const vec3& p1,const vec3& p2,const vec3& p3,
-						  const vec3& n1,const vec3& n2,const vec3& n3,vec3 c) //p1.y >= p2.y >= p3.y
-{
-	//cout << "n1=" << n1.x << "\t" << n1.y << "\t" << n1.z << endl;
-	//cout << "n2=" << n2.x << "\t" << n2.y << "\t" << n2.z << endl;
-	//cout << "n3=" << n3.x << "\t" << n3.y << "\t" << n3.z << endl;
-	int x_,y_=p1.y;
-	float x1,x2,z1,z2;
-	float z_;
-	float nx_,ny_,nz_,nx1,nx2,ny1,ny2,nz1,nz2;
-	while (y_ >= p3.y) {
-		if (p1.y == p2.y) {
-			//cout << "p1.y == p2.y" << endl;
-			if (p1.x < p2.x) {
-				x1 = p1.x + (p3.x-p1.x)/(p3.y-p1.y)*(y_-p1.y);
-				z1 = p1.z + (p3.z-p1.z)/(p3.y-p1.y)*(y_-p1.y);
-				x2 = p2.x + (p3.x-p2.x)/(p3.y-p2.y)*(y_-p2.y);
-				z2 = p2.z + (p3.z-p2.z)/(p3.y-p2.y)*(y_-p2.y);
-				if (shading==2) {
-					nx1 = n1.x + (n3.x-n1.x)/(p3.y-p1.y)*(y_-p1.y);
-					ny1 = n1.y + (n3.y-n1.y)/(p3.y-p1.y)*(y_-p1.y);
-					nz1 = n1.z + (n3.z-n1.z)/(p3.y-p1.y)*(y_-p1.y);
-					nx2 = n2.x + (n3.x-n2.x)/(p3.y-p2.y)*(y_-p2.y);
-					ny2 = n2.y + (n3.y-n2.y)/(p3.y-p2.y)*(y_-p2.y);
-					nz2 = n2.z + (n3.z-n2.z)/(p3.y-p2.y)*(y_-p2.y);
-				}
-			} else {
-				x1 = p2.x + (p3.x-p2.x)/(p3.y-p2.y)*(y_-p2.y);
-				z1 = p2.z + (p3.z-p2.z)/(p3.y-p2.y)*(y_-p2.y);
-				x2 = p1.x + (p3.x-p1.x)/(p3.y-p1.y)*(y_-p1.y);
-				z2 = p1.z + (p3.z-p1.z)/(p3.y-p1.y)*(y_-p1.y);
-				if (shading==2) {
-					nx2 = n1.x + (n3.x-n1.x)/(p3.y-p1.y)*(y_-p1.y);
-					ny2 = n1.y + (n3.y-n1.y)/(p3.y-p1.y)*(y_-p1.y);
-					nz2 = n1.z + (n3.z-n1.z)/(p3.y-p1.y)*(y_-p1.y);
-					nx1 = n2.x + (n3.x-n2.x)/(p3.y-p2.y)*(y_-p2.y);
-					ny1 = n2.y + (n3.y-n2.y)/(p3.y-p2.y)*(y_-p2.y);
-					nz1 = n2.z + (n3.z-n2.z)/(p3.y-p2.y)*(y_-p2.y);
-				}
-			}
-		} else if (p2.y == p3.y){ 
-			//cout << "p2.y == p3.y" << endl;
-			//x1 = x2 = p1.x;
-			if (p2.x < p3.x) {
-				x1 = p1.x + (p2.x-p1.x)/(p2.y-p1.y)*(y_-p1.y);
-				z1 = p1.z + (p2.z-p1.z)/(p2.y-p1.y)*(y_-p1.y);
-				x2 = p1.x + (p3.x-p1.x)/(p3.y-p1.y)*(y_-p1.y);
-				z2 = p1.z + (p3.z-p1.z)/(p3.y-p1.y)*(y_-p1.y);
-				if (shading==2) {
-					nx1 = n1.x + (n2.x-n1.x)/(p2.y-p1.y)*(y_-p1.y);
-					ny1 = n1.y + (n2.y-n1.y)/(p2.y-p1.y)*(y_-p1.y);
-					nz1 = n1.z + (n2.z-n1.z)/(p2.y-p1.y)*(y_-p1.y);
-					nx2 = n1.x + (n3.x-n1.x)/(p3.y-p1.y)*(y_-p1.y);
-					ny2 = n1.y + (n3.y-n1.y)/(p3.y-p1.y)*(y_-p1.y);
-					nz2 = n1.z + (n3.z-n1.z)/(p3.y-p1.y)*(y_-p1.y);
-				}
-			} else {
-				x1 = p1.x + (p3.x-p1.x)/(p3.y-p1.y)*(y_-p1.y);
-				z1 = p1.z + (p3.z-p1.z)/(p3.y-p1.y)*(y_-p1.y);
-				x2 = p1.x + (p2.x-p1.x)/(p2.y-p1.y)*(y_-p1.y);
-				z2 = p1.z + (p2.z-p1.z)/(p2.y-p1.y)*(y_-p1.y);
-				if (shading==2) {
-					nx1 = n1.x + (n3.x-n1.x)/(p3.y-p1.y)*(y_-p1.y);
-					ny1 = n1.y + (n3.y-n1.y)/(p3.y-p1.y)*(y_-p1.y);
-					nz1 = n1.z + (n3.z-n1.z)/(p3.y-p1.y)*(y_-p1.y);
-					nx2 = n1.x + (n2.x-n1.x)/(p2.y-p1.y)*(y_-p1.y);
-					ny2 = n1.y + (n2.y-n1.y)/(p2.y-p1.y)*(y_-p1.y);
-					nz2 = n1.z + (n2.z-n1.z)/(p2.y-p1.y)*(y_-p1.y);
-				}
-			}
-		}
-		for (x_=(int(x1)==x1)?x1:int(x1)+1; x_<=x2; x_++) {
-			z_ = z1 + (z2-z1)/(x2-x1)*(x_-x1);
-			if (shading==2) {
-				nz_ = nz1 + (nz2-nz1)/(x2-x1)*(x_-x1);
-				nx_ = nx1 + (nx2-nx1)/(x2-x1)*(x_-x1);
-				ny_ = ny1 + (ny2-ny1)/(x2-x1)*(x_-x1);
-			}
-			//cout << "nx_= " << nx_ << "\tny_= " << ny_ << "\tnz_=" << nz_ << endl;
-			if (shading==2) c = lighting(vec3(nx_,ny_,nz_));
-			framebuffer.draw(x_,y_,z_,c);
-		}
-		y_--;
-	}
-}
-
-void drawTriangle(vec3* triVertices,vec3* triNormals,vec3 c)  //triVertices & triNormal must have 3 element
-{
-	//use insertion sort to sort 3 Vertices with Y order
-	int vYSort[3];
-	for (int i=0; i<3; i++)
-		vYSort[i] = i;
-	for (int i=1; i<3; i++) {
-		int key = triVertices[vYSort[i]].y;
-		int temp = vYSort[i];
-		int j = i-1;
-		while (j>=0 && triVertices[vYSort[j]].y>key) {
-			vYSort[j+1] = vYSort[j];
-			j--;
-		}
-		vYSort[j+1] = temp;
-	}
-	//cout << "vYSort = " << vYSort[2] << "\t" << vYSort[1] << "\t" << vYSort[0] << endl; 
-	//cout << "p1:x= " << ix[vYSort[2]] << "\ty= " << iy[vYSort[2]] << "\tz= " << iz[vYSort[2]] << endl;
-	//cout << "p2:x= " << ix[vYSort[1]] << "\ty= " << iy[vYSort[1]] << "\tz= " << iz[vYSort[1]] << endl;
-	//cout << "p3:x= " << ix[vYSort[0]] << "\ty= " << iy[vYSort[0]] << "\tz= " << iz[vYSort[0]] << endl;
-	//cout << "n1:x= " << triNormals[vYSort[2]].x << "\ty= " << triNormals[vYSort[2]].y << "\tz= " << triNormals[vYSort[2]].z << endl;
-	//cout << "n2:x= " << triNormals[vYSort[1]].x << "\ty= " << triNormals[vYSort[1]].y << "\tz= " << triNormals[vYSort[1]].z << endl;
-	//cout << "n3:x= " << triNormals[vYSort[0]].x << "\ty= " << triNormals[vYSort[0]].y << "\tz= " << triNormals[vYSort[0]].z << endl;
-	if ((triVertices[vYSort[0]].y != triVertices[vYSort[1]].y) && 
-		(triVertices[vYSort[1]].y != triVertices[vYSort[2]].y) && 
-		(triVertices[vYSort[2]].y != triVertices[vYSort[0]].y)) {
-		float temp_x = triVertices[vYSort[0]].x + (triVertices[vYSort[2]].x-triVertices[vYSort[0]].x) / 
-					   (triVertices[vYSort[2]].y-triVertices[vYSort[0]].y) * (triVertices[vYSort[1]].y-triVertices[vYSort[0]].y);
-		float temp_z = triVertices[vYSort[0]].z + (triVertices[vYSort[2]].z-triVertices[vYSort[0]].z) / 
-					   (triVertices[vYSort[2]].y-triVertices[vYSort[0]].y) * (triVertices[vYSort[1]].y-triVertices[vYSort[0]].y);
-		vec3 p1(triVertices[vYSort[2]].x,triVertices[vYSort[2]].y,triVertices[vYSort[2]].z);
-		vec3 p2(triVertices[vYSort[1]].x,triVertices[vYSort[1]].y,triVertices[vYSort[1]].z);
-		vec3 p3(temp_x,triVertices[vYSort[1]].y,temp_z);
-		vec3 p4(triVertices[vYSort[0]].x,triVertices[vYSort[0]].y,triVertices[vYSort[0]].z);
-		vec3 n1,n2,n3,n4;
-		if (shading==2) {
-			temp_x = triNormals[vYSort[0]].x + (triNormals[vYSort[2]].x-triNormals[vYSort[0]].x) / 
-					 (triVertices[vYSort[2]].y-triVertices[vYSort[0]].y) * (triVertices[vYSort[1]].y-triVertices[vYSort[0]].y);
-			float temp_y = triNormals[vYSort[0]].y + (triNormals[vYSort[2]].y-triNormals[vYSort[0]].y) / 
-						   (triVertices[vYSort[2]].y-triVertices[vYSort[0]].y) * (triVertices[vYSort[1]].y-triVertices[vYSort[0]].y);
-			temp_z = triNormals[vYSort[0]].z + (triNormals[vYSort[2]].z-triNormals[vYSort[0]].z) / 
-					 (triVertices[vYSort[2]].y-triVertices[vYSort[0]].y) * (triVertices[vYSort[1]].y-triVertices[vYSort[0]].y);
-			n1 = vec3(triNormals[vYSort[2]].x,triNormals[vYSort[2]].y,triNormals[vYSort[2]].z);
-			n2 = vec3(triNormals[vYSort[1]].x,triNormals[vYSort[1]].y,triNormals[vYSort[1]].z);
-			n3 = vec3(temp_x,temp_y,temp_z);
-			n4 = vec3(triNormals[vYSort[0]].x,triNormals[vYSort[0]].y,triNormals[vYSort[0]].z);
-		} else {
-			n1 = n2 = n3 = n4 = vec3(0.f);
-		}
-		drawStandingTriangle(p1,p2,p3,n1,n2,n3,c);
-		drawStandingTriangle(p2,p3,p4,n2,n3,n4,c);
-	} else if ((triVertices[vYSort[0]].y==triVertices[vYSort[1]].y) && (triVertices[vYSort[1]].y==triVertices[vYSort[2]].y)) {
-	} else {
-		vec3 p1(triVertices[vYSort[2]].x,triVertices[vYSort[2]].y,triVertices[vYSort[2]].z);
-		vec3 p2(triVertices[vYSort[1]].x,triVertices[vYSort[1]].y,triVertices[vYSort[1]].z);
-		vec3 p3(triVertices[vYSort[0]].x,triVertices[vYSort[0]].y,triVertices[vYSort[0]].z);
-		vec3 n1(triNormals[vYSort[2]].x,triNormals[vYSort[2]].y,triNormals[vYSort[2]].z);
-		vec3 n2(triNormals[vYSort[1]].x,triNormals[vYSort[1]].y,triNormals[vYSort[1]].z);
-		vec3 n3(triNormals[vYSort[0]].x,triNormals[vYSort[0]].y,triNormals[vYSort[0]].z);
-		drawStandingTriangle(p1,p2,p3,n1,n2,n3,c);
-	}
-}
-
-void drawEdge(int* ix,int* iy,float* iz,vec3 c)  //ix,iy,iz must have 3 element
-{
-	float m[3],B[3];
-	for (int j=0; j<3; j++) {
-		int k = j+1;
-		if (k==3) k = 0;
-		if (abs(iy[k]-iy[j]) <= abs(ix[k]-ix[j])) {	//use y=mx+B
-			m[j] = float(iy[k]-iy[j])/float(ix[k]-ix[j]);
-			B[j] = iy[j]-m[j]*ix[j];
-			if (ix[j]<ix[k]) {
-				for (int x_=ix[j]; x_<=ix[k]; x_++) {
-					int y_ = x_*m[j]+B[j];
-					float z_ = iz[j] + (iz[k]-iz[j])/(ix[k]-ix[j])*(x_-ix[j]);
-					framebuffer.draw(x_,y_,z_,c);
-				}
-			} else {
-				for (int x_=ix[k]; x_<=ix[j]; x_++) {
-					int y_ = x_*m[j]+B[j];
-					float z_ = iz[k] + (iz[j]-iz[k])/(ix[j]-ix[k])*(x_-ix[k]);
-					framebuffer.draw(x_,y_,z_,c);
-				}
-			}
-		} else {									//use x=my+B
-			m[j] = float(ix[k]-ix[j])/float(iy[k]-iy[j]);
-			B[j] = ix[j]-m[j]*iy[j];
-			if (iy[j]<iy[k]) {
-				for (int y_=iy[j]; y_<=iy[k]; y_++) {
-					int x_ = y_*m[j]+B[j];
-					float z_ = iz[j] + (iz[k]-iz[j])/(iy[k]-iy[j])*(y_-iy[j]);
-					framebuffer.draw(x_,y_,z_,c);
-				}
-			} else {
-				for (int y_=iy[k]; y_<=iy[j]; y_++) {
-					int x_ = y_*m[j]+B[j];
-					float z_ = iz[k] + (iz[j]-iz[k])/(iy[j]-iy[k])*(y_-iy[k]);
-					framebuffer.draw(x_,y_,z_,c);
-				}
-			}
-		}
-	}
-}
-
 void displayFunc() 
 {
 	framebuffer.clear();
 
-	/* draw point cloud */
-	/* TODO: change the following code to do rasterization */
-	//int ix, iy;
-	//int screenSide = min(screenWidth, screenHeight);
-	//float margin = 0.1f;
-	//float prevX, prevY, prevZ, curX, curY, curZ;
-	//cout << modelPtr[curModelIdx]->numVertices << endl;
-	/*for(int i = 1; i <= modelPtr[curModelIdx]->numVertices; ++i) {       //drawing vertices
-		prevX = modelPtr[curModelIdx]->vertices[3*i  ];
-		prevY = modelPtr[curModelIdx]->vertices[3*i+1];
-		prevZ = modelPtr[curModelIdx]->vertices[3*i+2];
-
-		// rotate along x-axis
-		curY = prevY*cos(thetaX)-prevZ*sin(thetaX);
-		curZ = prevY*sin(thetaX)+prevZ*cos(thetaX);
-		// prevY = curY;
-		prevZ = curZ;
-		// rotate along y-axis
-		curX = prevZ*sin(thetaY)+prevX*cos(thetaY);
-		curZ = prevZ*cos(thetaY)-prevX*sin(thetaY);
-		// draw the framebuffer
-		ix = int(((1.f-margin)*curX)*screenSide) + screenWidth_half - 1;
-		iy = int(((1.f-margin)*curY)*screenSide) + screenHeight_half - 1;
-		cout << "ix = " << ix << endl;
-		cout << "iy = " << iy << endl;
-		cout << "iz = " << curZ << endl;
-		framebuffer.draw(ix, iy, curZ, vec3(1.f));
-	}*/
 	for (int i=0; i<modelPtr[curModelIdx]->numTriangles; i++) {
+		//Reading vertices and normals of Triangle
 		Triangle* triangle = modelPtr[curModelIdx]->triangles;
 		float* vertice = modelPtr[curModelIdx]->vertices;
 		float* normal = modelPtr[curModelIdx]->normals;
@@ -406,9 +130,7 @@ void displayFunc()
 				triNormals[j].z = 0;
 			}
 		}
-		//cout << "n1:x= " << triNormals[0].x << "\ty= " << triNormals[0].y << "\tz= " << triNormals[0].z << endl;
-		//cout << "n2:x= " << triNormals[1].x << "\ty= " << triNormals[1].y << "\tz= " << triNormals[1].z << endl;
-		//cout << "n3:x= " << triNormals[2].x << "\ty= " << triNormals[2].y << "\tz= " << triNormals[2].z << endl;
+
 		//Transform
 		triVertices[0] = transform(triVertices[0]);
 		triVertices[1] = transform(triVertices[1]);
@@ -418,13 +140,7 @@ void displayFunc()
 			triNormals[1] = transform(triNormals[1]);
 			triNormals[2] = transform(triNormals[2]);
 		}
-		/*int ix[3],iy[3];
-		float iz[3];
-		toScreenSpace(triVertices[0],ix[0],iy[0],iz[0]);
-		toScreenSpace(triVertices[1],ix[1],iy[1],iz[1]);
-		toScreenSpace(triVertices[2],ix[2],iy[2],iz[2]);
-		drawTriangle(ix,iy,iz);
-		drawEdge(ix,iy,iz,vec3(1.f,0.f,0.f));*/
+
 		//Back Face Culling
 		if (!backFaceCulling(triVertices) || !culling || wireframe_filled==0) {
 			int ix[3],iy[3];
@@ -446,6 +162,7 @@ void displayFunc()
 			if (wireframe_filled==1) drawTriangle(triVertices,triNormals,c);
 			else drawEdge(ix,iy,iz,vec3(1.f,0.f,0.f));
 		}
+
 		//for Debug purpose (will draw back face culling wireframe)
 		/*else {
 			int ix[3],iy[3];
---
+++ src/transform.cpp
@@ -0,0 +1,50 @@
+#include "framebuffer.h"
+#include "transform.h"
+#include <cmath>
+
+using namespace std;
+
+extern double thetaX;
+extern double thetaY;
+
+extern int screenWidth;
+extern int screenHeight;
+extern int screenWidth_half;
+extern int screenHeight_half;
+
+vec3 transform(vec3& prev)
+{
+	float curX,curY,curZ;
+	// rotate along x-axis
+	curY = prev.y*cos(thetaX)-prev.z*sin(thetaX);
+	curZ = prev.y*sin(thetaX)+prev.z*cos(thetaX);
+	// prevY = curY;
+	prev.z = curZ;
+	// rotate along y-axis
+	curX = prev.z*sin(thetaY)+prev.x*cos(thetaY);
+	curZ = prev.z*cos(thetaY)-prev.x*sin(thetaY);
+	return vec3(curX,curY,curZ);
+}
+
+void toScreenSpace(vec3& v,int& ix,int& iy,float& iz)
+{
+	int screenSide = min(screenWidth, screenHeight);
+	float margin = 0.1f;
+	// to ScreenSpace
+	ix = int(((1.f-margin)*v.x)*screenSide) + screenWidth_half - 1;
+	iy = int(((1.f-margin)*v.y)*screenSide) + screenHeight_half - 1;
+	iz = v.z;
+}
+
+bool backFaceCulling(vec3* triangleV)   //input 3 vertex of the triangle
+{
+	vec3 v1(triangleV[0].x-triangleV[1].x,triangleV[0].y-triangleV[1].y,triangleV[0].z-triangleV[1].z);
+	vec3 v2(triangleV[0].x-triangleV[2].x,triangleV[0].y-triangleV[2].y,triangleV[0].z-triangleV[2].z);
+	vec3 v3 = crossProduct(v1,v2);
+	normalize(v3);
+	vec3 v4(0.f,0.f,-1.f);
+	//cout << dotProduct(v3,v4) << endl;
+	if (dotProduct(v3,v4) < 0) return true;
+	else return false;
+}
+
