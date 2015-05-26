deaaf6aad535b582e3a65b8c58911aa78595ffda
Ching-Ting Chang <c62066206@hotmail.com>
2015-05-26 12:20:42 +0800

    Transform
    1. Add translate key (see help)
    2. Chande mode key (see help)

--- src/transform.cpp
+++ src/transform.cpp
@@ -1,6 +1,7 @@
 #include "framebuffer.h"
 #include "transform.h"
 #include <cmath>
+#include <glm/gtc/matrix_transform.hpp>
 
 using namespace std;
 
@@ -18,7 +19,9 @@ extern int screenHeight;
 extern int screenWidth_half;
 extern int screenHeight_half;
 
-vec3 transform(vec3& prev)
+extern bool projection;
+
+void model2view_rotation(vec3& prev)
 {
 	float curX,curY,curZ;
 	// rotate along x-axis
@@ -29,17 +32,58 @@ vec3 transform(vec3& prev)
 	// rotate along y-axis
 	curX = (prev.z*sin(thetaY)+prev.x*cos(thetaY))*(1+deltaSize)+deltaX;
 	curZ = (prev.z*cos(thetaY)-prev.x*sin(thetaY))*(1+deltaSize)+deltaZ;
-	return vec3(curX,curY,curZ);
+	prev.x = curX;
+	prev.y = curY;
+	prev.z = curZ;
+}
+
+glm::mat4 modelMatrix(glm::vec3 translation,glm::vec3 rotation,double scale) {
+	glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f),rotation.x,glm::vec3(1.0f,0.0f,0.0f));
+			  rotationMatrix = glm::rotate(rotationMatrix,rotation.y,glm::vec3(0.0f,1.0f,0.0f));
+			  rotationMatrix = glm::rotate(rotationMatrix,rotation.z,glm::vec3(0.0f,0.0f,1.0f));
+	glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0f),glm::vec3(scale));
+	glm::mat4 translateMatrix = glm::translate(glm::mat4(1.0f),translation);
+	return rotationMatrix * scaleMatrix * translateMatrix;
+}
+
+glm::mat4 rotationMatrix(glm::vec3 rotation) {
+	glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f),rotation.x,glm::vec3(1.0f,0.0f,0.0f));
+			  rotationMatrix = glm::rotate(rotationMatrix,rotation.y,glm::vec3(0.0f,1.0f,0.0f));
+			  rotationMatrix = glm::rotate(rotationMatrix,rotation.z,glm::vec3(0.0f,0.0f,1.0f));
+	return rotationMatrix;
+}
+
+glm::mat4 viewMatrix(glm::vec3 cameraPos,glm::vec3 cameraTarget,glm::vec3 upVector) {
+	glm::mat4 cameraMatrix = glm::lookAt(cameraPos,cameraTarget,upVector);
+	return cameraMatrix;
+}
+
+glm::mat4 projectionMatrix(float FoV) {
+	glm::mat4 projectionMatrix(1.0f);
+	if (projection==0) {
+		 projectionMatrix = glm::perspective(FoV,1.0f/1.0f,0.1f,100.0f);
+	} else {
+		projectionMatrix = glm::ortho(10.0f,10.0f,10.0f,10.0f,0.1f,100.0f);
+	}
+	return projectionMatrix;
 }
 
 void toScreenSpace(vec3& v,int& ix,int& iy,float& iz)
 {
 	int screenSide = min(screenWidth, screenHeight);
 	float margin = 0.1f;
-	// to ScreenSpace
-	ix = int(((1.f-margin)*v.x)*screenSide) + screenWidth_half - 1;
-	iy = int(((1.f-margin)*v.y)*screenSide) + screenHeight_half - 1;
-	iz = v.z;
+	v.z += 1;
+	if (projection==0) {
+		// Orthogonal Projection
+		ix = int(((1.f-margin)*v.x)*screenSide) + screenWidth_half - 1;
+		iy = int(((1.f-margin)*v.y)*screenSide) + screenHeight_half - 1;
+		iz = v.z;
+	} else {
+		//Perspective Projection
+		ix = (((1.f-margin)*v.x/v.z*0.5)*screenSide) + screenWidth_half - 1;
+		iy = (((1.f-margin)*v.y/v.z*0.5)*screenSide) + screenHeight_half - 1;
+		iz = v.z;
+	}
 }
 
 bool backFaceCulling(vec3* triangleV)   //input 3 vertex of the triangle
