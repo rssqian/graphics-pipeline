0a7722a31a6098fafd32af83b36aeca01ef01dc5
tanyuan <tsy754@gmail.com>
2015-05-26 10:36:08 +0800

    Less model so not crash

--- src/main.cpp
+++ src/main.cpp
@@ -35,10 +35,10 @@ vec3 color(1.f);
 
 /* model names */
 char* modelNames[] = {
-	"model/quad.obj",
-	"model/couch.obj",
-	"model/blaze.obj",
-	"model/ateneal.obj",
+	//"model/quad.obj",
+	//"model/couch.obj",
+	//"model/blaze.obj",
+	//"model/ateneal.obj",
 	"model/venusm.obj",
 	"model/bunnyC.obj",
 	"model/duck4KN.obj",
